6a2048c517f71e49b64a90fd437af576ebb087a9
Hajar <alhasanhajar@gmail.com>
2021-07-20 13:30:52 -0400

    Initial commit

---
+++ src/test/java/com/tts/fullstackdemo/FullstackDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tts.fullstackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+// we need to comment the annotation out because
+// this will attempt to provision a database
+// which is impossible with our github actions workflow
+
+//@SpringBootTest     for the gradle.yml
+class FullstackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
