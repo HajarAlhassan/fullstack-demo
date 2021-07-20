348d0192347eeb2ad402599888102e2c48f1f1a8
Hajar <alhasanhajar@gmail.com>
2021-07-20 13:48:43 -0400

    git commit - "push code with assertTrue equal false it will fail"

---
+++ src/test/java/com/tts/fullstackdemo/MyTest.java
@@ -0,0 +1,11 @@
+package com.tts.fullstackdemo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;// for the assertTrue method
+
+public class MyTest {
+    @Test
+    public void test(){
+        assertTrue(false);
+    }
+}
